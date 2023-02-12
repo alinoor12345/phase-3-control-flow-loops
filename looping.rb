5acd403962be20d6ff7769f84c0c67290a1a954f
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2023-02-12 21:00:50 +0300

    lab complete

--- looping.rb
+++ looping.rb
@@ -1,9 +1,16 @@
 def happy_new_year
   # your code here
-end
-
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
-def fizzbuzz(num)
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
+ end
+ 
+ 
+ # No need to modify this code! Use this to implement the fizzbuzz_printer method.
+ def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
   elsif num % 3 == 0
@@ -13,12 +20,31 @@ def fizzbuzz(num)
   else
     num
   end
-end
-
-def fizzbuzz_printer
+ end
+ 
+ 
+ def fizzbuzz_printer
   # your code here
-end
-
-def reverse_string(str)
+  (1..100).each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts num
+   end
+  end
+ end
+ 
+ 
+ def reverse_string(str)
   # your code here
-end
+  reversed = ""
+  (str.length - 1).downto(0) do |i|
+  reversed << str[i]
+  end
+  return reversed
+ end
+
